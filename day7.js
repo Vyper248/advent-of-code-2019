33c35e31622bbf0e94ffb33431cd54de50327db2
Vyper248 <vyper375@hotmail.com>
2019-12-09 11:53:13 +0000

    Day 7 Done

---
+++ day7.js
@@ -0,0 +1,170 @@
+const readFile = require('./readFile').readFile;
+
+const runFunctions = (input) => {
+    let firstStar = first(input);
+    let secondStar = second(input);
+
+    console.log('First Star: ', firstStar);
+    console.log('Second Star: ', secondStar);
+}
+
+const first = (input) => {   
+    let arr = input.split(',').map(Number);
+    let combinations = getCombinations([0,1,2,3,4]);
+    let max = 0;
+
+    combinations.forEach(sequence => {
+        let currentResult = 0;
+        for (let i = 0; i < 5; i++) currentResult = runProgram([...arr], sequence[i], currentResult);
+        if (currentResult > max) max = currentResult;
+    });
+
+    return max;
+};
+
+const second = (input) => {
+    let arr = input.split(',').map(Number);
+    let combinations = getCombinations([5,6,7,8,9]);
+    let max = 0;
+
+    combinations.forEach(sequence => {
+        let programs = getPrograms(arr, sequence);
+
+        let finished = false;
+        let first = true;
+        let currentOutput = null;
+        let finalOutput = 0;
+
+        while (finished === false) {
+            finished = true;
+            
+            for (let i = 0; i < programs.length; i++) {
+                let program = programs[i];
+
+                if (first) {
+                    currentOutput = program.next(0); //start first program with 0 input and move to output
+                    program.next(); //move to next input request
+                    first = false;
+                } else {
+                    currentOutput = program.next(currentOutput.value); //input value from previous output
+                    program.next(); //move to next input request
+                }
+
+                if (currentOutput.done === false) finished = false; //if all programs aren't finished, keep going
+                if (i === programs.length-1 && currentOutput.value !== undefined) finalOutput = currentOutput.value;
+            }
+        }
+
+        if (finalOutput > max) max = finalOutput;      
+    });
+
+    return max;
+};
+
+//get programs and start with phase settings input
+const getPrograms = (arr, sequence) => {
+    let programs = [];
+    for (let i = 0; i < sequence.length; i++) {
+        let program = runProgramGen([...arr]);
+        program.next(); //move to first input
+        program.next(sequence[i]); //input phase setting and move to next input
+        programs.push(program);
+    }
+    return programs;
+}
+
+const getCombinations = (input) => {
+    let arr = [];
+
+    const func = (possibleValues, currentValues) => {
+        for (let i = 0; i < possibleValues.length; i++) {
+            let values = [...currentValues, possibleValues[i]];
+            let newArr = possibleValues.filter((a) => a !== possibleValues[i]);
+            if (newArr.length > 0) func(newArr, values);
+            if (possibleValues.length === 1) arr.push(values);
+        }
+    }
+
+    func(input, []);
+    
+    return arr;
+}
+
+const runProgram = (arr, firstInput, secondInput) => {
+    const program = runProgramGen(arr);
+    program.next();
+    program.next(firstInput);
+    let output = program.next(secondInput);
+    return output.value;
+}
+
+function *runProgramGen(arr) {
+    let length = 4;
+    let finalOutput = 0;
+
+    for (let i = 0; i < arr.length; i+=length) {        
+        let [opcode, paramA, paramB] = getParams(arr[i]);  
+
+        if (opcode === 99) break;
+
+        length = 4;
+        let inputA = arr[i+1];
+        let inputB = arr[i+2];
+        let output = arr[i+3];
+
+        let inputAVal = paramA === 0 ? arr[inputA] : inputA;
+        let inputBVal = paramB === 0 ? arr[inputB] : inputB;
+
+        let error = false;
+             
+        switch (opcode) {
+            case 1: arr[output] = inputAVal + inputBVal; break; //add
+            case 2: arr[output] = inputAVal * inputBVal; break; //multiply
+            case 3: arr[arr[i+1]] = yield; length = 2; break; //input
+            case 4: finalOutput = paramA === 0 ? yield arr[arr[i+1]] : yield arr[i+1]; length = 2; break; //output
+            case 5: inputAVal !== 0 ? [i, length] = [inputBVal, 0] : length = 3; break; //jump-if-true
+            case 6: inputAVal === 0 ? [i, length] = [inputBVal, 0] : length = 3; break; //jump-if-false
+            case 7: inputAVal < inputBVal ? arr[output] = 1 : arr[output] = 0; break; //less than
+            case 8: inputAVal === inputBVal ? arr[output] = 1 : arr[output] = 0; break; //equal
+            default: error = true; break;
+        }
+
+        if (error) {
+            console.log('Error, opcode was: ', opcode);
+            break;
+        }        
+    }
+
+    return finalOutput;
+}
+
+const getParams = (num) => {
+    if (num < 100) return [num, 0, 0];
+
+    let parts = (num+'').split('').map(Number);
+    
+    let opCode = Number(`${parts[parts.length-2]}${parts[parts.length-1]}`);
+    let paramA = parts[parts.length-3] === undefined ? 0 : parts[parts.length-3];
+    let paramB = parts[parts.length-4] === undefined ? 0 : parts[parts.length-4];
+
+    return [opCode, paramA, paramB];
+}
+
+//====================================================== TESTING ======================================================
+
+const test = (input, expectedValue) => {
+    if (first(input) === expectedValue) console.log(true);
+    else console.log(false);
+}
+
+const testSecond = (input, expectedValue) => {
+    if (second(input) === expectedValue) console.log(true);
+    else console.log(false);
+}
+
+// test('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', 43210);
+// testSecond('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5', 139629729);
+
+//==================================================== READ FILE =====================================================
+
+readFile('day7Input.txt', runFunctions);
